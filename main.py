c572b7a03b8eeeb42382810820ba98fac979cb2a
tarik <trk.kck95@gmail.com>
2021-07-10 23:19:34 +0300

    revers

---
+++ main.py
@@ -0,0 +1,13 @@
+
+#liste elemanlarını tersine çeviren elemanlarından liste olan varsa onlarında yani nested listelerinde
+#elemanlarını da tersine çeviren program
+def rev(l):
+    l.reverse()
+    for i in range(len(l)):
+        if type(l[i]) == list:
+            l[i].reverse()
+
+    return l
+
+bosliste = []
+liste = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
